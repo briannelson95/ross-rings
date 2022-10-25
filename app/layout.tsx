489515cab7808b24e64a665c9a2f8319996edbb8
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-10-25 17:51:37 -0400

    init and studio setup

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// These styles apply to every route in the application
+import './global.css';
+
+export default function RootLayout({ children }: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+const STUDIO_REWRITE = {
+  source: "/admin/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/admin/:path*"
+      : "/admin/index.html",
+}
+
 const nextConfig = {
+  rewrites: () => [STUDIO_REWRITE],
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
 }
 
 module.exports = nextConfig
