02c8cbd0aad25e778023c7726dbaf4a1a7359b16
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-10-25 19:43:03 -0400

    getStaticProps is gone and it's dumb

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
+import { client } from "../lib/sanity";
+import { homepage } from "../lib/queries";
+
+export async function getData() {
+    const res = await client.fetch(homepage);
+
+    return res.json()
+}
+
+export default async function Page() {
+    const data = await getData();
+    console.log(data)
     return <h1 className="text-2xl font-bold">Hello, Next.js!</h1>;
 }
+
