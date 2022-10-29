f81f5f2b25de6e23aaa2019aa3dfd93a9e071816
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-10-29 08:17:14 -0400

    added next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [{ userAgent: '*', disallow: '/' }]
+    }
+  }
+  
