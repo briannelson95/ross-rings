8992b304773d41fa7f9f8d564214c7e30c1568fd
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-10-28 22:02:01 -0400

    Revert "hope this doesnt break thing"
    This reverts commit 3521138a92584c6762f7352f70aa793260096597.

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    generateRobotsTxt: true,
-    robotsTxtOptions: {
-        policies: [{ userAgent: '*', disallow: '/' }]
-    }
-  }
