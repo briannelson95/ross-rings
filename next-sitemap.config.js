2dd88f12d337f6ef1422af2b3d5e968fb72b1d39
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-10-29 08:27:31 -0400

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
+    siteUrl: 'https://ross-rings.vercel.app/',
+    // generateIndexSitemap: false,
     generateRobotsTxt: true,
     robotsTxtOptions: {
         policies: [{ userAgent: '*', disallow: '/' }]
