e831fb769d03b6aa133bd9817bfc0a3ed4fd1587
Brian Nelson <71141646+briannelson95@users.noreply.github.com>
2022-12-01 09:06:42 -0500

    Update HeroBanner.tsx

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -17,7 +17,7 @@ const HeroBanner = ({ image, filter, title, children }) => {
                 <div className={`w-full ${filter ? 'mix-blend-color bg-pale-sky' : ''}`}>
                     <div className="h-full flex justify-center items-center" />
                 </div>
-                <div className="lg:ml-44 2xl:ml-64 absolute h-full flex items-center">
+                <div className="px-10 2xl:mx-52 absolute h-full flex items-center">
                     {title 
                         ?  <h1 className={`lg:text-[112px] 2xl:text-[133px] ${filter ? 'text-white' : ''}`}>{title}</h1>
                         : children}
