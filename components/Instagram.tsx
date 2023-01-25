1834800bc6be2366ac43987e9a9015859d8a834d
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-01-25 09:14:43 -0500

    remove link from social feed

--- components/Instagram.tsx
+++ components/Instagram.tsx
@@ -6,12 +6,12 @@ export default function Instagram({ items }){
     <div className="productsRow grid grid-cols-1 gap-4 lg:grid-cols-4 lg:grid-rows-2">
       {items.map((item, index) => (
         <>
-          <a
+          <div
               className="relative h-72 w-full col-span-1 lg:col-span-1 row-span-1 first:lg:col-start-1 lg:first:col-span-2 lg:first:row-span-2 lg:first:h-full"
               key={index}
-              href={item.permalink}
-              target='_blank'
-              rel="noreferrer"
+              // href={item.permalink}
+              // target='_blank'
+              // rel="noreferrer"
           >
               <div className="absolute w-full h-full overflow-hidden">
                   <Image
@@ -22,7 +22,7 @@ export default function Instagram({ items }){
                       className='w-full h-full object-cover transition ease-in-out duration-300 hover:scale-110'
                   />
               </div>
-          </a>
+          </div>
         </>
       ))}
     </div>
